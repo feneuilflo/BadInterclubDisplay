a49fca8277c49c8c620b6d026762978ccdd96547
fenflo81 <florent.feneuil@laposte.net>
2018-12-27 12:47:35 +0100

    improved match score display

--- src/main/java/com/bad/interclub/display/bach/ui/controllers/MatchScoreController.java
+++ src/main/java/com/bad/interclub/display/bach/ui/controllers/MatchScoreController.java
@@ -1,17 +1,28 @@
 package com.bad.interclub.display.bach.ui.controllers;
 
 import com.bad.interclub.display.bach.model.Match;
+import com.bad.interclub.display.bach.model.Player;
+import com.bad.interclub.display.bach.model.ScoreUtils;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableIntegerValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class MatchScoreController implements Initializable {
 
+    @FXML
+    private Region rgnPlayers;
 
     @FXML
     private Label lblHostPlayer;
@@ -20,10 +31,16 @@ public class MatchScoreController implements Initializable {
     private Label lblGuestPlayer;
 
     @FXML
+    private Region rgnSet1;
+
+    @FXML
     private Label lblHostSet1;
 
     @FXML
     private Label lblGuestSet1;
+
+    @FXML
+    private Region rgnSet2;
 
     @FXML
     private Label lblHostSet2;
@@ -32,10 +49,18 @@ public class MatchScoreController implements Initializable {
     private Label lblGuestSet2;
 
     @FXML
+    private Region rgnSet3;
+
+    @FXML
     private Label lblHostSet3;
 
     @FXML
     private Label lblGuestSet3;
+
+    private IntegerProperty winnerSet1 = new SimpleIntegerProperty();
+    private IntegerProperty winnerSet2 = new SimpleIntegerProperty();
+    private IntegerProperty winnerSet3 = new SimpleIntegerProperty();
+    private IntegerProperty score = new SimpleIntegerProperty();
 
     private ChangeListener<Number> listenerHostSet1;
     private ChangeListener<Number> listenerHostSet2;
@@ -49,12 +74,15 @@ public class MatchScoreController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // setup listeners
-        listenerHostSet1 = (observable, oldValue, newValue) -> lblHostSet1.setText(String.valueOf(newValue));
-        listenerHostSet2 = (observable, oldValue, newValue) -> lblHostSet2.setText(String.valueOf(newValue));
-        listenerHostSet3 = (observable, oldValue, newValue) -> lblHostSet3.setText(String.valueOf(newValue));
-        listenerGuestSet1 = (observable, oldValue, newValue) -> lblGuestSet1.setText(String.valueOf(newValue));
-        listenerGuestSet2 = (observable, oldValue, newValue) -> lblGuestSet2.setText(String.valueOf(newValue));
-        listenerGuestSet3 = (observable, oldValue, newValue) -> lblGuestSet3.setText(String.valueOf(newValue));
+        listenerHostSet1 = (observable, oldValue, newValue) -> lblHostSet1.setText(scoreToString(newValue.intValue()));
+        listenerHostSet2 = (observable, oldValue, newValue) -> lblHostSet2.setText(scoreToString(newValue.intValue()));
+        listenerHostSet3 = (observable, oldValue, newValue) -> lblHostSet3.setText(scoreToString(newValue.intValue()));
+        listenerGuestSet1 = (observable, oldValue, newValue) -> lblGuestSet1.setText(scoreToString(newValue.intValue()));
+        listenerGuestSet2 = (observable, oldValue, newValue) -> lblGuestSet2.setText(scoreToString(newValue.intValue()));
+        listenerGuestSet3 = (observable, oldValue, newValue) -> lblGuestSet3.setText(scoreToString(newValue.intValue()));
+
+        // players background
+        rgnPlayers.setStyle("-fx-background-color: linear-gradient(to bottom, -host-color-1 0%, -host-color-2 47%, #600060 50%, -guest-color-2 53%, -guest-color-1 100%)");
     }
 
     public void setMatch(Match match) {
@@ -66,9 +94,19 @@ public class MatchScoreController implements Initializable {
             this.match.getScore().getSet2().guestPointsProperty().removeListener(listenerGuestSet2);
             this.match.getScore().getSet3().hostPointsProperty().removeListener(listenerHostSet3);
             this.match.getScore().getSet3().guestPointsProperty().removeListener(listenerGuestSet3);
+
+            rgnPlayers.styleProperty().unbind();
+            rgnSet1.styleProperty().unbind();
+            rgnSet2.styleProperty().unbind();
+            rgnSet3.styleProperty().unbind();
         }
 
-        // add listeners
+        // setup background
+        rgnSet1.styleProperty().bind(fromScoreToBackground(ScoreUtils.getWinnerProperty(match.getScore().getSet1())));
+        rgnSet2.styleProperty().bind(fromScoreToBackground(ScoreUtils.getWinnerProperty(match.getScore().getSet2())));
+        rgnSet3.styleProperty().bind(fromScoreToBackground(ScoreUtils.getWinnerProperty(match.getScore().getSet3())));
+
+        // add listeners for score
         this. match = match;
         this.match.getScore().getSet1().hostPointsProperty().addListener(listenerHostSet1);
         this.match.getScore().getSet1().guestPointsProperty().addListener(listenerGuestSet1);
@@ -78,15 +116,30 @@ public class MatchScoreController implements Initializable {
         this.match.getScore().getSet3().guestPointsProperty().addListener(listenerGuestSet3);
 
         // fill current score
-        lblHostSet1.setText(String.valueOf(this.match.getScore().getSet1().getHostPoints()));
-        lblHostSet2.setText(String.valueOf(this.match.getScore().getSet2().getHostPoints()));
-        lblHostSet3.setText(String.valueOf(this.match.getScore().getSet3().getHostPoints()));
-        lblGuestSet1.setText(String.valueOf(this.match.getScore().getSet1().getGuestPoints()));
-        lblGuestSet2.setText(String.valueOf(this.match.getScore().getSet2().getGuestPoints()));
-        lblGuestSet3.setText(String.valueOf(this.match.getScore().getSet3().getGuestPoints()));
+        lblHostSet1.setText(scoreToString(this.match.getScore().getSet1().getHostPoints()));
+        lblHostSet2.setText(scoreToString(this.match.getScore().getSet2().getHostPoints()));
+        lblHostSet3.setText(scoreToString(this.match.getScore().getSet3().getHostPoints()));
+        lblGuestSet1.setText(scoreToString(this.match.getScore().getSet1().getGuestPoints()));
+        lblGuestSet2.setText(scoreToString(this.match.getScore().getSet2().getGuestPoints()));
+        lblGuestSet3.setText(scoreToString(this.match.getScore().getSet3().getGuestPoints()));
 
         // fill players name
+        lblHostPlayer.setWrapText(match.getHost().size() == 1); // wrap text for single matches only
+        lblGuestPlayer.setWrapText(match.getGuest().size() == 1);
         lblHostPlayer.setText(match.getHost().stream().map(pl -> pl.getSurname().toUpperCase() + " " + pl.getFirstname()).collect(Collectors.joining("\n")));
         lblGuestPlayer.setText(match.getGuest().stream().map(pl -> pl.getSurname().toUpperCase() + " " + pl.getFirstname()).collect(Collectors.joining("\n")));
+    }
+
+    private static String scoreToString(int score) {
+        return score > 0 ? String.valueOf(score) : "-";
+    }
+
+    private StringBinding fromScoreToBackground(ObservableIntegerValue score) {
+        return Bindings.createStringBinding(() -> getScoreBackgroundColor(score.get()), score);
+    }
+
+    private String getScoreBackgroundColor(int score) {
+        return String.format("-fx-background-color: linear-gradient(to bottom, -host-color-1 0%%, -host-color-2 %d%%, #600060 %d%%, -guest-color-2 %d%%, -guest-color-1 100%%);",
+                47 + score * 12, 50 + score * 12, 53 + score * 12);
     }
 }
